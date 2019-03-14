854fbc4159a239d8a8ef8f5a6916cda06c97fb14
Rodion Kharabet <rodion.kharabet@ims-ukraine.com>
2019-03-14 13:11:39 +0200

    Add Nordicsoft_ee and change folder tree

---
+++ Readme.md
@@ -0,0 +1,1 @@
+NordicSoft projects
---
+++ src/Nordicsofte_ee/Nordicsofte_ee.Web/Services/GoogleRecaptcha/IGoogleRecaptcha.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Nordicsoft_ee.Web.Services.GoogleRecaptcha
+{
+    public interface IGoogleRecaptcha
+    {
+        Task<bool> IsCaptchaValid(string encodedResponse, string action);
+    }
+}
