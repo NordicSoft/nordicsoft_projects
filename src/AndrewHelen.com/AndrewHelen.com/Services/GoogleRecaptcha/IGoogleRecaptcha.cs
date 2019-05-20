84cfeb4b3989e2cc816174a0b9bce1d69da738d6
Rodion Kharabet <pensioneer4ik@gmail.com>
2019-05-20 14:24:50 +0300

    Remove other projects from branch

--- src/AndrewHelen.com/AndrewHelen.com/Services/GoogleRecaptcha/IGoogleRecaptcha.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace AndrewHelen.com.Services.GoogleRecaptcha
-{
-    public interface IGoogleRecaptcha
-    {
-        Task<bool> IsCaptchaValid(string encodedResponse, string action);
-    }
-}
--- src/Nordicsoft.events/NordicSoftEvents/Services/MailSender/IMailSender.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Event.Services.MailSender
-{
-    public interface IMailSender
-    {
-        Task<bool> SendEmailAsync(IMail mail);
-    }
-}
