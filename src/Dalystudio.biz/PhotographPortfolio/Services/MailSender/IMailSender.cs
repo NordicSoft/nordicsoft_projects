cde35666ba3a14cc4289f634b2d0d4fe62913a8d
Rodion Kharabet <pensioneer4ik@gmail.com>
2019-05-20 14:26:21 +0300

    Remove other projects from branch

--- src/Dalystudio.biz/PhotographPortfolio/Services/MailSender/IMailSender.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace PhotographPortfolio.Services.MailSender
-{
-    public interface IMailSender
-    {
-        Task<bool> SendEmailAsync(IMail mail);
-    }
-}
--- src/RichardGrace.com/RichardGrace.com/Services/GoogleRecaptcha/IGoogleRecaptcha.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace RichardGrace.com.Services.GoogleRecaptcha
-{
-    public interface IGoogleRecaptcha
-    {
-        Task<bool> IsCaptchaValid(string encodedResponse, string action);
-    }
-}
