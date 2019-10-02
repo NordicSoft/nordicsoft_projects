ae293b180ea4f3ffe4b4afbb715f17067f111c3c
Anna Shpak01 <anna.shpak@nordicsoft.team>
2019-10-02 15:53:47 +0300

    Dalystudio.biz - portfolio centered icon hover

--- src/Dalystudio.biz/Dalystudio.biz/ClientApp/src/assets/components/counters/counters.jsx
+++ src/Dalystudio.biz/Dalystudio.biz/ClientApp/src/assets/components/counters/counters.jsx
@@ -1,121 +1,85 @@
 ﻿"use strict";
-import React, { Component } from 'react'
-import "./counters.css"
 
-import CountTo from 'react-count-to';
-import CountUp from 'react-countup';
+import React, { Component } from 'react';
+import CountUp, { startAnimation } from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
 
-class CounterBar extends Comment {
+class CounterBar extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = { value: 0 };
+        //this.buttonClicked = this.buttonClicked.bind(this);
+        this.eventScroll = this.eventScroll.bind(this);
+    }
+    componentDidMount() {
+        document.getElementById("counters").addEventListener('scroll', function () {
+            this.eventScroll.bind(this);
+        });
     }
 
+    eventScroll(ev)  {
+        this.setState({ value: this.props.val });
+    }
 
+    render() {
+        return (
+            <div className="col-sm-3">
+                <div>
+                <div>{this.state.value}</div>
+                    <button onClick={this.eventScroll.bind(this)}>Click</button>
+                    </div>
+                </div>
+        );
+    }
 }
-
-
-//class CounterBar extends React.Component {
-//    constructor(props) {
-//        super(props);
-//    }
-
-//    componentDidMount() {
-        
-//    }
-
-//    render(){
-//        const fn = value => <span>{value}</span>;
-
-//    const counterArray = [
-//        { id: 1, number: "49", name: "countries visited" },
-//        { id: 2, number: "1356", name: "total customers" },
-//        { id: 3, number: "71", name: "photos made" },
-//        { id: 4, number: "788", name: "albums created" }
-//    ];
-
-//    const counters = [];
-
-//        for (var i = 0; i < 4; i++) {
-//            if (i == 2) {
-//                counters.push(<div className="col-sm-3 m-b-md-20">
-//                    <div className="counter-item">
-
-//                        <div key={counterArray[i].id} className="counter-number font-alt">
-//                            <CountTo to={counterArray[i].number} speed={1500}>{fn}</CountTo>
-//                            <span>K</span>
-//                            <div className="counter-title font-inc">{counterArray[i].name}</div>
-//                        </div>
-//                    </div>
-//                </div>);
-//            }
-//            else {
-
-//                counters.push(<div className="col-sm-3 m-b-md-20">
-//                    <div className="counter-item">
-
-//                        <div key={counterArray[i].id} className="counter-number font-alt">
-//                            <CountTo to={counterArray[i].number} speed={1000}>{fn}</CountTo>
-//                            <div className="counter-title font-inc">{counterArray[i].name}</div>
-//                        </div>
-//                    </div>
-//                </div>);
-//            }
-//        }
-//    return(counters);
-// }
-
-//}
-//const CompleteHook = () => {
-//    const { countUp, start, pauseResume, reset, update } = useCountUp({
-//        start: 0,
-//        end: 1234567,
-//        delay: 1000,
-//        duration: 5,
-//        onReset: () => console.log('Resetted!'),
-//        onUpdate: () => console.log('Updated!'),
-//        onPauseResume: () => console.log('Paused or resumed!'),
-//        onStart: ({ pauseResume }) => console.log(pauseResume),
-//        onEnd: ({ pauseResume }) => console.log(pauseResume),
-//    });
-//    return (
-//        <div>
-//            <div>{countUp}</div>
-//            <button onClick={start}>Start</button>
-//            <button onClick={reset}>Reset</button>
-//            <button onClick={pauseResume}>Pause/Resume</button>
-//            <button onClick={() => update(2000)}>Update to 2000</button>
-//        </div>
-//    );
-//};
+                    //<CounterBar val={30}/>
+                    //<CounterBar val={90}/>
+                    //<CounterBar val={73}/>
+                    //<CounterBar val={24}/>
 function Counter() {
     const bgAbout = "../img/about.jpg";
-    return (
-        <div className="module-small about-section bg-dark bg-dark-50 lazyload"/* data-bg={bgAbout}*/>
 
-            <div className="container">
+    //function isVisible(elem) {
 
-                <div className="row">
+    //    let coords = elem.getBoundingClientRect();
+    //    let windowHeight = document.documentElement.clientHeight;
+    //    let topVisible = coords.top > 0 && coords.top < windowHeight;
+    //    let bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
 
-                    <CountUp
-                        start={0}
-                        end={160527.012}
-                        duration={2.75}
-                        separator=" "
-                        decimals={4}
-                        decimal="," >
-                        {({ countUpRef, start }) => (
-                            <div>
-                                <span ref={countUpRef} />
-                                <button onClick={start}>Start</button>
-                            </div>
-                        )}
-                    </CountUp>
-               
-                </div>
+    //    return topVisible || bottomVisible;
+    //}
 
-            </div>
+   
 
-        </div>
+    //function showVisible() {
+    //    var block = document.getElementById("counters");
+
+    //        if (isVisible(block)) {
+
+    //            var innerBlock = " <CountUp start={0} end={100}> { ({ countUpRef, start }) => ( <div className=\"container\"> <div className=\"row\"> <div className=\"col-sm-3\"><div><span ref={countUpRef} /><button onClick={start} > click </button> </div></div></div></div>)}</CountUp >";
+
+
+    //            block.innerHTML = innerBlock;
+    //        }
+        
+    //}
+
+    //window.addEventListener('scroll', showVisible);
+    //showVisible();
+
+
+    return (<div className="module-small about-section bg-dark bg-dark-50 lazyload" data-bg={bgAbout} id="counters" >
+        <div className="container"><div className="row">
+            <div className="col-sm-3">
+                <div className="counter-item">
+                    <div className="counter-number font-alt">
+              <span>  <CountUp start={0} end={100}> </CountUp></span> 
+                        <div class="counter-title font-inc">
+                            countries visited
+                        </div>
+            </div> </div>
+            </div> 
+        </div> </div></div>
     );
 }
 
