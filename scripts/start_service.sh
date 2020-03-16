58c6f80d6181d2c94e47bc0b131ac8c945f1ae8b
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-03-16 13:20:41 +0200

    Update start scripts

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -1,2 +1,5 @@
 service richard-grace_com restart & 
-service andrew-helen_com restart
+service andrew-helen_com restart &
+service nordicsoft_events restart &
+service dalystudio_biz restart &
+service nordicsoft_ee restart &
