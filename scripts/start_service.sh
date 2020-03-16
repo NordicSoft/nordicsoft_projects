c3356f87df1812687870abfdea0f9191a4e65988
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-03-16 13:31:18 +0200

    Remove trailing ampersand

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -2,4 +2,4 @@ service richard-grace_com restart &
 service andrew-helen_com restart &
 service nordicsoft_events restart &
 service dalystudio_biz restart &
-service nordicsoft_ee restart &
+service nordicsoft_ee restart
