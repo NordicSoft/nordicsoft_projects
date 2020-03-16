c46b4de52395b5199e4c74561c490a321f3898d5
Rodion Kharabet <pensioneer4ik@gmail.com>
2020-03-16 14:42:58 +0200

    Update dalystudio

--- src/Dalystudio.biz/Dalystudio.biz/TagHelpers/BreadcrumbsTagHelper.cs
+++ src/Dalystudio.biz/Dalystudio.biz/TagHelpers/BreadcrumbsTagHelper.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
-using Nordicsoft_ee.Extensions;
 using SmartBreadcrumbs;
 
 namespace Dalystudio.biz.TagHelpers
